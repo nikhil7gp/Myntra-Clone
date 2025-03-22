c6a796180e110cfb2c525269bacb59fff2b53341
Nikhil Gopalputti <129383651+nikhil7gp@users.noreply.github.com>
2025-03-22 20:09:12 +0530

    Add files via upload

---
+++ 1-react-myntra-clone/src/components/HomeItem.jsx
@@ -0,0 +1,55 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bagActions } from "../store/bagSlice";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { MdDeleteForever } from "react-icons/md";
+
+const HomeItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const bagItem = useSelector((state) => state.bag);
+  const elementFound = bagItem.indexOf(item.id) >= 0;
+
+  const handleAddToCart = () => {
+    dispatch(bagActions.addToBag(item.id));
+  };
+
+  const handleRemove = () => {
+    dispatch(bagActions.removeFromBag(item.id));
+  };
+
+  return (
+    <div className="item-container">
+      <img className="item-image" src={item.image} alt="item image" />
+      <div className="rating">
+        {item.rating.stars} ⭐ | {item.rating.count}
+      </div>
+      <div className="company-name">{item.company}</div>
+      <div className="item-name">{item.item_name}</div>
+      <div className="price">
+        <span className="current-price">Rs {item.current_price}</span>
+        <span className="original-price">Rs {item.original_price}</span>
+        <span className="discount">({item.discount_percentage}% OFF)</span>
+      </div>
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-danger"
+          onClick={handleRemove}
+        >
+          <MdDeleteForever />
+          Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-success"
+          onClick={handleAddToCart}
+        >
+          <IoIosAddCircleOutline />
+          Add to Bag
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default HomeItem;
---
+++ 1-react-myntra-clone/src/components/LoadingSpinner.jsx
@@ -0,0 +1,17 @@
+const LoadingSpinner = () => {
+  return (
+    <h1>
+      <div className="d-flex justify-content-center spinner">
+        <div
+          className="spinner-border"
+          role="status"
+          style={{ width: "4rem", height: "4rem" }}
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </h1>
+  );
+};
+
+export default LoadingSpinner;
